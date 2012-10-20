84435a3e230b6041aa8f6f126b785de94d7de4b8
klehigh <klehigh@iu.edu>
2012-10-20 11:56:32 -0400

    add bash default variables for dl tool and args
    - add DLTOOL variable and define curl as default value to support alternate download tools.
    
    - add DLARGS variable and define default values for curl to support alternate download tools

--- update_bro_intel.sh
+++ update_bro_intel.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-# download Bro intel file and update current file.
 # Author : Keith Lehigh <klehigh@iu.edu>
+# download Bro intel file and overwrite current file.
 
 # URL for our intel file
 WEBPATH='https://path/to/web/intel.txt'
@@ -9,8 +9,15 @@ WEBPATH='https://path/to/web/intel.txt'
 # Location of Bro Intel File
 INTELFILE='/usr/local/bro/share/bro/site/intel.txt'
 
-curl -s $WEBPATH >$INTELFILE
+# if, for some reason, you do not want to use curl to download the intel file
+# uncomment and define DLTOOL and DLARGS 
+#DLTOOL=blah
+#DLARGS="-b -l -a -h"
 
+# run download tool and overwrite existing intel file
+${DLTOOL:-curl} ${DLARGS:--s} $WEBPATH >$INTELFILE
+
+# check our return value and bail if we fail
 if [ $? -ne 0 ];
 then
 	echo "Download of Bro Intel failed!  Exiting!"
