4164756f5588ad8e380a0cd552e8359dea9dd6d8
klehigh <klehigh@iu.edu>
2012-10-20 11:51:04 -0400

    add more comments

--- create_bro_intel.sh
+++ create_bro_intel.sh
@@ -2,16 +2,19 @@
 
 # Author : Keith Lehigh <klehigh@iu.edu>
 # Pull feeds from SES.  output in Bro format.
-# Your API key should be stored in $HOME/.cif
+# API key should be stored in $HOME/.cif of user running script
 
+# space separated list of feeds
 # colon separated feed name and confidence value.
 FEEDS="infrastructure/botnet:85 domain/botnet:85 url/botnet:85"
+# define path to our intel file
 OUTFILE="/path/on/webserver/intel.txt"
 
 # copy last output file to .bak extension, just in case
 if [ -e $OUTFILE ];
 then
 	cp $OUTFILE $OUTFILE.bak
+	# check return value and quit if we failed to copy
 	if [ $? -ne 0 ];
 	then
 		echo "Failed to make backup of intel file.  Bailing out!"
@@ -19,12 +22,13 @@ then
 	fi
 fi
 
-# let's truncate $OUTFILE via a lazy echo
+# truncate $OUTFILE via a lazy echo
 echo -n >$OUTFILE
 
 # download CIF data in Bro format
 for pair in $FEEDS;
 do
+	# redefine our separator and assign split values to numbered variables
 	IFS=:; set $pair
 	cif -q $1 -c $2 -p bro >>$OUTFILE
 	if [ $? -ne 0 ];
